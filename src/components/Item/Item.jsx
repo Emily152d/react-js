fe06b2b620274d39c85a56e16f648801429dd428
Esteban Hirzfeld <estebanhirzfeld@gmail.com>
2024-08-27 22:29:04 -0300

    fix: remove unused key

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const Item = ({item}) => {
     return (
         <Link to={`/item/${item.id}`}>
-            <div key={item.id} className='card'>
+            <div className='card'>
                 <h2>{item.name}</h2>
                 <img src={item.image} alt={item.name} className='hola' />
             </div>
